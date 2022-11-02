024b7676bf47ee56944842b0bc6add0d012863c3
nevermin9 <chizzza@gmail.com>
2022-11-02 12:02:09 +0100

    init base styles; add fonts

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
+import './styles/styles.scss'
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import PrimeVue from 'primevue/config'
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(PrimeVue)
+    .mount('#app')
